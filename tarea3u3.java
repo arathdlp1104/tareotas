782ab36494ee83f0a9f936506217b6ac62e689fa
arathdlp1104 <l24121341@morelia.tecnm.mx>
2024-10-15 21:54:02 -0600

    Create tarea3u3.java

---
+++ tarea3u3.java
@@ -0,0 +1,74 @@
+ boolean condicionTres = 3 < 5 && 7 >= 2 ;
+       boolean condicionCuatro =10 != 8 || 15 == 15;
+       boolean condicionCinco = !(5 < 6) && 2 <= 3 ;
+       boolean condicionSeis = (8 == 8 || 3 < 1) && 6 > 2 ;
+        boolean condicionSiete = 12 != 10 && (4 < 8 || 9 == 9) ;
+        boolean condicionocho =(3 + 5) == 8 || (7 * 2) <= 14 ;
+       boolean condicionnueve = !(9 <= 5) && (4 == 4 || 6 != 6) ;
+        boolean condiciondies = 10 % 2 == 0 && (5 > 1 || 12 != 10) ;
+        boolean condiciononce = (3 * 3 == 9) && !(8 < 5 || 7 >= 9) ;
+       boolean condiciondose = !(5 + 5 != 10) && (2 * 3 > 5 || 6 == 7);
+        boolean condiciontrece =20 / 4 == 5 || !(10 % 3 == 1 && 4 >= 3) ;
+        boolean condicioncatorce = !(7 > 3 && 2 == 2) || (5 * 2 < 12 && 8 >= 8) ;
+        boolean condicionquince = (15 - 5 > 8) && (3 == 3 || 7 < 1) && 12 >= 10 ;
+        boolean condiciondiesiseis = 9 % 3 == 0 && (8 <= 12 || 5 + 5 != 10) && (7 > 8) ;
+        boolean condiciondiesisiete = !(10 != 10) && (5 * 3 == 15 || 2 < 1) && 9 >= 9 ;
+        boolean condiciondiesiocho =(4 + 6 == 10 || 2 == 3) && !(8 < 8 && 3 > 2) || 7 % 2 == 1;
+              boolean condiciondiesinueve =  (12 / 3 > 4) && (5 == 5 || 10 != 10) && !(8 >= 9) ;
+       boolean condicionveinte = !(7 + 3 < 10) && (9 / 3 == 3 || 6 < 2) && (5 * 4 >= 20) ;
+                 boolean condicionveintiuno =((10 - 2) != 8) && !(6 >= 7 || 2 == 2) && (12 % 3 == 0) ;
+        boolean condicionveintidos =!(15 > 5 && 3 + 2 == 5) || (9 < 10 && !(6 <= 4)) && (7 * 2 != 14) ;
+
+
+
+
+        if (condicionTres == true) {
+            System.out.println("condicion correcta");
+        } else {
+            System.out.println("condicion incorrecta");
+        }
+
+
+        if (condicionCuatro == true) {
+            System.out.println("condicion correcta");
+        } else { System.out.println("condicion incorrecta"); }
+
+
+        if (condicionCinco == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+
+        if (condicionSeis == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicionSiete == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicionocho == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicionnueve == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciondies == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciononce == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciondose == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciontrece == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicioncatorce == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicionquince == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciondiesiseis == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciondiesisiete == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciondiesiocho == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condiciondiesinueve == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicionveinte == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicionveintiuno == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+        if (condicionveinte == true) { System.out.println("condicion correcta"); }
+        else { System.out.println("condicion incorrecta"); }
+
